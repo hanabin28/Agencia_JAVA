80680e829f36e798510d5abe726edf2b8077b755
Vitoria <vazevedo73@gmail.com>
2023-09-27 17:25:28 -0300

    finalizando transferencia

--- src/br/senai/sp/jandira/model/Conta.java
+++ src/br/senai/sp/jandira/model/Conta.java
@@ -1,15 +1,49 @@
 package br.senai.sp.jandira.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Conta {
     boolean validaSaldo;
-    private int numeroConta = 1234;
-    private double saldo = 9652;
+    private int numeroConta;
+    private double saldo = 0;
     private String agencia = "000-35-XX";
+
+    private Cliente cliente;
+
+    List<Conta> listConta = new ArrayList<>();
+
+    public void adicionarContaList(Conta conta){
+        listConta.add(conta);
+    }
+
+    public Conta pesquisarConta(long cpf){
+        for (Conta conta: listConta){
+            long validaCpf = conta.cliente.getCpf();
+
+            if (validaCpf == cpf){
+                return conta;
+            }
+        }
+        return null;
+    }
+
+    public void gerarConta(Cliente cliente){
+        numeroConta = (int) (Math.random()*10000);
+        this.cliente = cliente;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 
     public void realizarDeposito(Double valor){
         this.saldo += valor;
         System.out.println("O saldo da sua conta é: " + this.saldo);
-        System.out.println();
     }
 
     public void realizarSaque(Double valor){
@@ -17,21 +51,36 @@ public class Conta {
             this.saldo -= valor;
             System.out.println("Seu saldo é de:" + this.saldo);
         }else {
-            System.out.println("não pode!!!");
+            System.out.println("Você não tem saldo para essa transação.");
         }
     }
 
     public void validarSaque(Double valor){
-        if (valor>this.saldo){
+        if (valor > this.saldo){
             validaSaldo = false;
         }else {
-            this.saldo -= valor;
-            validaSaldo = true;
+            validaSaldo = false;
         }
     }
 
     public void consultarSaldo(){
         System.out.println("Seu saldo é: " + this.saldo);
-        System.out.println();
+    }
+
+    public void realizarTransferencia(Double valor){
+        if (validaSaldo) {
+            this.saldo -= valor;
+            System.out.println("Seu saldo é de:" + this.saldo);
+        }else {
+            System.out.println("Você não tem saldo para essa transação.");
+        }
+    }
+
+    public void validarTransferencia(Double valor){
+        if (valor > this.saldo){
+            validaSaldo = false;
+        }else {
+            validaSaldo = false;
+        }
     }
 }
